73c252f2a625f53ed6300098876ec13428804b21
idail <neto_br_8@hotmail.com>
2024-07-25 11:14:36 -0400

    JS de backup

---
+++ visao/assets/js/backup_js.js
@@ -0,0 +1,66 @@
+// if (
+          //   listaNomeClientes.includes(
+          //     retorno_vendas[vendas].nome_cliente_venda
+          //   )
+          // ) {
+          //   console.log("nome ja consta no array");
+          // } else {
+          //   listaNomeClientes.push(retorno_vendas[vendas].nome_cliente_venda);
+          // }
+
+          // listaImagensProdutos.push(retorno_vendas[vendas].nome_produto_venda);
+
+          // let recebeValorVendaBR =
+          //   retorno_vendas[vendas].valor_final_venda.toString();
+
+          // let recebeValorVendaBRFinal =
+          //   "R$" + recebeValorVendaBR.replace(".", ",");
+
+          // let recebeValorDescontoV = retorno_vendas[vendas].desconto_venda;
+
+          // let recebePagoV = retorno_vendas[vendas].pago_venda;
+
+          // let recebeAgendamentoV =
+          //   retorno_vendas[vendas].pagamento_agendado_venda;
+
+          // let recebeValorDescontoVString = "";
+          // let recebeValorDescontoBRFinal = "";
+
+          // if (retorno_vendas[vendas].desconto_final_venda != null) {
+          //   recebeValorDescontoVString =
+          //     retorno_vendas[vendas].desconto_final_venda.toString();
+          //   recebeValorDescontoBRFinal =
+          //     "R$" + recebeValorDescontoVString.replace(".", ",");
+          // } else {
+          //   recebeValorDescontoBRFinal = "Não informado";
+          // }
+
+          // let recebeValorFinalDescontoV = "";
+
+          // if (recebeValorDescontoV === 1) recebeValorFinalDescontoV = "Sim";
+          // else recebeValorFinalDescontoV = "Não";
+
+          // let recebeValorPagoV = "";
+
+          // if (recebePagoV === 1) recebeValorPagoV = "Sim";
+          // else recebeValorPagoV = "Não";
+
+          // let recebeValorAgendamentoV = "";
+
+          // if (recebeAgendamentoV === 1) recebeValorAgendamentoV = "Sim";
+          // else recebeValorAgendamentoV = "Não";
+
+          // let recebeDataAgendamentoV = "";
+          // let recebeDataAmericana = "";
+          // let recebeDataBRAgendamentoV = "";
+
+          // if (retorno_vendas[vendas].data_pagamento_venda != null) {
+          //   recebeDataAgendamentoV =
+          //     retorno_vendas[vendas].data_pagamento_venda;
+          //   recebeDataBRAgendamentoV = recebeDataAgendamentoV
+          //     .split("-")
+          //     .reverse()
+          //     .join("/");
+          // } else {
+          //   recebeDataBRAgendamentoV = "Não informado";
+          // }
