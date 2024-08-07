9ea3fba53fe3c776a5b0761d123201f7b5280f82
idail <neto_br_8@hotmail.com>
2024-08-07 00:47:18 -0400

    Implementações
    Sendo implementado o esqueci minha senha

---
+++ visao/acesso/alterar_senha.php
@@ -0,0 +1,50 @@
+<div class="modal fade" id="esqueci-minha-senha-usuario" tabindex="-1" data-bs-backdrop="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card mb-3">
+
+                    <div class="card-body">
+
+                        <div class="pt-4 pb-2">
+                            <h5 class="card-title text-center pb-0 fs-4">Recuperação de senha</h5>
+                            <p class="text-center small">Insira o e-mail para alteração de senha</p>
+                        </div>
+
+                        <form class="row g-3" id="formulario-cadastro-usuario" novalidate>
+                            <div class="col-12">
+                                <label for="yourName" class="form-label">E-mail</label>
+                                <input type="text" name="email-usuario" class="form-control" id="email-usuario" required>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show" id="recebe-mensagem-alteracao-senha-realizado-usuario" role="alert">
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="alert alert-warning bg-warning border-0 alert-dismissible fade show" id="recebe-mensagem-campo-vazio-alteracao-senha-usuario" role="alert">
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="alert alert-warning bg-warning border-0 alert-dismissible fade show" id="recebe-mensagem-campo-falha-alteracao-senha-usuario" role="alert">
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
--- visao/acesso/login.php
+++ visao/acesso/login.php
@@ -105,7 +105,7 @@
 
                     <div class="col-12">
                       <p class="small mb-0">Não possui uma conta?<a href="#" data-bs-toggle="modal" data-bs-target="#cadastro-usuario" data-backdrop="static">Cadastrar Usuario</a></p>
-                      <p class="small mt-2">Esqueceu sua senha?<a href="#" data-bs-toggle="modal" data-bs-target="" data-backdrop="static">Clique aqui</a></p>
+                      <p class="small mt-2">Esqueceu sua senha?<a href="#" data-bs-toggle="modal" data-bs-target="#esqueci-minha-senha-usuario" data-backdrop="static">Clique aqui</a></p>
                     </div>
                   </form>
 
@@ -133,6 +133,7 @@
 
               <?php
               require("cadastro_usuario.php");
+              require("alterar_senha.php");
               ?>
 
 
